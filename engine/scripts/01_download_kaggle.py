653587f2124889962d3f764ca3b70e839e6cda7d
MarcoVetrano01 <marco.vetrano01@gmail.com>
2026-03-20 19:35:38 +0100

    Deleted unnecessary print in 01_download_kaggle.py file

--- engine/scripts/01_download_kaggle.py
+++ engine/scripts/01_download_kaggle.py
@@ -8,7 +8,6 @@ def download_and_copy_dataset():
         print("Downloading dataset from Kaggle...")
         path = kh.dataset_download("olistbr/brazilian-ecommerce")
         destination_path = os.path.join(os.getcwd(), "Dataset_Raw")
-        print(destination_path)
         shutil.copytree(path, destination_path, dirs_exist_ok=True)
         return f'Dataset downloaded and copied successfully in {destination_path}.'
     
